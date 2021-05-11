923a9f21c5cbcb074a1dd5f08970571ece8509b4
JY-Wei <75135154+JY-Wei@users.noreply.github.com>
2021-05-11 20:28:55 +0800

    Update digitalclock.js

--- digitalclock.js
+++ digitalclock.js
@@ -1,19 +1,28 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s ;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+
+  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
+
+  var t = setTimeout(startTime, 500);
+  n = new Date();
+  y = n.getFullYear();
+  m = n.getMonth() + 1;
+  d = n.getDate();
+
+  document.getElementById("date").innerHTML = m + " " + d + " " + y;
 }
 
-showTime();
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+setInterval(() => {
+  document.title = new Date().toLocaleTimeString();
+}, 1);
