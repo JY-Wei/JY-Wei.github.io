ad3dc68836565bb489cf8e8a0ebd1f21caf5090a
JY-Wei <75135154+JY-Wei@users.noreply.github.com>
2021-05-11 20:02:55 +0800

    Update digitalclock.js

--- digitalclock.js
+++ digitalclock.js
@@ -3,22 +3,18 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
+    var y = date.getFullYear();
+    var mo = date.getMonth() + 1;
+    var d = date.getDate();
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s ;
+    var time = mo + " " + d + " " + y + " " + h + ":" + m + ":" + s ;
     
-    var t = setTimeout(startTime, 500);
-    n = new Date();
-    y = n.getFullYear();
-    mo = n.getMonth() + 1;
-    d = n.getDate();
-    var time2 = mo + " " + d + " " + y + " " + time;
-    
-    document.getElementById("MyClockDisplay").innerText = time2;
-    document.getElementById("MyClockDisplay").textContent = time2;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
     
     setTimeout(showTime, 1000);
     
